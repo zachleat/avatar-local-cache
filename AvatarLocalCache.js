0c6814413a47b62071e6320660e65d4580b46378
Zach Leatherman <zachleatherman@gmail.com>
2019-08-23 09:07:46 -0500

    Don't convert images if they're already in a format. Use default quality settings.

--- AvatarLocalCache.js
+++ AvatarLocalCache.js
@@ -56,118 +56,141 @@ class AvatarLocalCache {
 			}).then(res => res.buffer()).then(function(body) {
 				let promises = [];
 				let img = sharp(body).resize({ width: width, withoutEnlargement: true });
+				img.metadata().catch(function(error) {
+					fetchReject(`Url: ${url}\nError: ${error}`);
+				}).then(function(metadata) {
+					if(this.formats.indexOf("jpeg") > -1) {
+						let jpgPromise = new Promise((resolve, reject) => {
+							// http://sharp.pixelplumbing.com/en/stable/api-output/#jpeg
+							let jpeg;
+							if(metadata.format !== "jpeg") {
+								jpeg = img.jpeg();
+							} else {
+								jpeg = img;
+							}
 
-				if(this.formats.indexOf("jpeg") > -1) {
-					let jpgPromise = new Promise((resolve, reject) => {
-						// http://sharp.pixelplumbing.com/en/stable/api-output/#jpeg
-						img.jpeg({ quality: 100 })
-							.toFile(`${outputFileSlug}.jpg`, (err, info) => {
-								if(err) {
-									fetchReject(`Url: ${url}\nError: ${err}`);
-								} else {
-									imagemin([`${outputFileSlug}.jpg`], {
-										plugins: [
-											imageminJpegtran()
-										]
-									}).then(files => {
-										for(let file of files) {
-											fs.writeFile(`${outputFileSlug}.jpg`, file.data, err => {
-												if(err) {
-													fetchReject(`Url: ${url}\nError: ${err}`);
-												} else {
-													resolve(AvatarLocalCache.getReturnObject(outputFileSlug, "jpg"));
-												}
-											});
-										}
-									});
-								}
-							});
-					});
+							jpeg.toFile(`${outputFileSlug}.jpg`, (err, info) => {
+									if(err) {
+										fetchReject(`Url: ${url}\nError: ${err}`);
+									} else {
+										imagemin([`${outputFileSlug}.jpg`], {
+											plugins: [
+												imageminJpegtran()
+											]
+										}).then(files => {
+											for(let file of files) {
+												fs.writeFile(`${outputFileSlug}.jpg`, file.data, err => {
+													if(err) {
+														fetchReject(`Url: ${url}\nError: ${err}`);
+													} else {
+														resolve(AvatarLocalCache.getReturnObject(outputFileSlug, "jpg"));
+													}
+												});
+											}
+										});
+									}
+								});
+						});
 
-					promises.push(jpgPromise);
-				}
-
-				if(this.formats.indexOf("png") > -1) {
-					let pngPromise = new Promise((resolve, reject) => {
-						// http://sharp.pixelplumbing.com/en/stable/api-output/#png
-						img.png()
-							.toFile(`${outputFileSlug}.png`, (err, info) => {
-								if(err) {
-									fetchReject(`Url: ${url}\nError: ${err}`);
-								} else {
-									// console.log( `Wrote ${outputFileSlug}.png` );
-
-									imagemin([`${outputFileSlug}.png`], {
-										plugins: [
-											imageminPngquant()
-										]
-									}).then(files => {
-										for(let file of files) {
-											fs.writeFile(`${outputFileSlug}.png`, file.data, err => {
-												if(err) {
-													fetchReject(`Url: ${url}\nError: ${err}`);
-												} else {
-													resolve(AvatarLocalCache.getReturnObject(outputFileSlug, "png"));
-												}
-											});
-										}
-									});
-								}
-							});
-					});
-
-					promises.push(pngPromise);
-				}
-
-				if(this.formats.indexOf("webp") > -1) {
-					let webpPromise = new Promise((resolve, reject) => {
-						// http://sharp.pixelplumbing.com/en/stable/api-output/#webp
-						img.webp({ lossless: true })
-							.toFile(`${outputFileSlug}.webp`, (err, info) => {
-								if(err) {
-									fetchReject(`Url: ${url}\nError: ${err}`);
-								} else {
-									resolve(AvatarLocalCache.getReturnObject(outputFileSlug, "webp"));
-								}
-							});
-					});
-
-					promises.push(webpPromise);
-				}
-
-				Promise.all(promises).then(function(files) {
-					let sorted = files.sort(function(a, b) {
-						return a.size - b.size;
-					});
-
-					if(this.onlyKeepSmallestFormats) {
-						// remove webp if it’s not the smallest
-						if(!sorted[0].path.endsWith(".webp")) {
-							sorted = sorted.filter(function(entry) {
-								if(entry.path.endsWith(".webp")) {
-									fs.unlink(entry.path, (err) => {
-										if (err) throw err;
-									});
-									return false;
-								} else {
-									return true;
-								}
-							});
-						}
-
-						// remove the biggest of png/jpg
-						if( sorted.filter(function(entry) {
-							return entry.path.endsWith(".jpg") || entry.path.endsWith(".png");
-						}).length >= 2 ) {
-							let cut = sorted.pop();
-							fs.unlink(cut.path, (err) => {
-								if (err) throw err;
-							});
-						}
+						promises.push(jpgPromise);
 					}
 
-					fetchResolve(sorted);
-				}.bind(this));
+					if(this.formats.indexOf("png") > -1) {
+						let pngPromise = new Promise((resolve, reject) => {
+							// http://sharp.pixelplumbing.com/en/stable/api-output/#png
+							let png;
+							if(metadata.format !== "png") {
+								png = img.png();
+							} else {
+								png = img;
+							}
+
+							png.toFile(`${outputFileSlug}.png`, (err, info) => {
+									if(err) {
+										fetchReject(`Url: ${url}\nError: ${err}`);
+									} else {
+										// console.log( `Wrote ${outputFileSlug}.png` );
+
+										imagemin([`${outputFileSlug}.png`], {
+											plugins: [
+												imageminPngquant()
+											]
+										}).then(files => {
+											for(let file of files) {
+												fs.writeFile(`${outputFileSlug}.png`, file.data, err => {
+													if(err) {
+														fetchReject(`Url: ${url}\nError: ${err}`);
+													} else {
+														resolve(AvatarLocalCache.getReturnObject(outputFileSlug, "png"));
+													}
+												});
+											}
+										});
+									}
+								});
+						});
+
+						promises.push(pngPromise);
+					}
+
+					if(this.formats.indexOf("webp") > -1) {
+						let webpPromise = new Promise((resolve, reject) => {
+							// http://sharp.pixelplumbing.com/en/stable/api-output/#webp
+							let webp;
+							if(metadata.format !== "webp") {
+								webp = img.webp();
+							} else {
+								webp = img;
+							}
+
+							webp.toFile(`${outputFileSlug}.webp`, (err, info) => {
+									if(err) {
+										fetchReject(`Url: ${url}\nError: ${err}`);
+									} else {
+										resolve(AvatarLocalCache.getReturnObject(outputFileSlug, "webp"));
+									}
+								});
+						});
+
+						promises.push(webpPromise);
+					}
+
+					Promise.all(promises).then(function(files) {
+						let sorted = files.sort(function(a, b) {
+							return a.size - b.size;
+						});
+
+						if(this.onlyKeepSmallestFormats) {
+							// remove webp if it’s not the smallest
+							if(!sorted[0].path.endsWith(".webp")) {
+								sorted = sorted.filter(function(entry) {
+									if(entry.path.endsWith(".webp")) {
+										fs.unlink(entry.path, (err) => {
+											if (err) throw err;
+										});
+										return false;
+									} else {
+										return true;
+									}
+								});
+							}
+
+							// remove the biggest of png/jpg
+							if( sorted.filter(function(entry) {
+								return entry.path.endsWith(".jpg") || entry.path.endsWith(".png");
+							}).length >= 2 ) {
+								let cut = sorted.pop();
+								fs.unlink(cut.path, (err) => {
+									if (err) throw err;
+								});
+							}
+						}
+
+						fetchResolve(sorted);
+
+					}.bind(this));
+
+				}.bind(this)); // .metadata
 			}.bind(this));
 		});
 	}
